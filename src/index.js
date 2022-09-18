053d3bf622e8fbd3c2b079c6946af40c1999ec84
bit1100 <surajkeshari1100@gmail.com>
2022-09-18 11:45:24 +0530

    HashRouter to BrowserROuter Replaced with basename prop in it

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </React.StrictMode>
